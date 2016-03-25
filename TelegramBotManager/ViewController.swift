98cfc5ccf26ac77dc559524ffef74a51de56041c
Alessandro Maroso <alessandro.maroso@iks.it>
2016-03-25 14:47:41 +0100

    Initial Commit

---
+++ TelegramBotManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TelegramBotManager
+//
+//  Created by Alessandro Maroso on 25/03/16.
+//  Copyright © 2016 membersheep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
